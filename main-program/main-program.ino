c264498101390561d6890c7c9725bf64b098eacb
TE4-Daniel-Kull <daniel.kull@elev.ga.ntig.se>
2020-04-22 16:46:02 +0200

    Added keyboard buffer

--- main-program/main-program.ino
+++ main-program/main-program.ino
@@ -1,5 +1,9 @@
 const int inputPin = 2;
 
+uint8_t buf[8] = { 
+  0 };   /* Keyboard report buffer */
+bool oldResult = false;
+
 void setup() {
   pinMode(inputPin, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -9,7 +13,28 @@ void setup() {
 
 void loop() {
   bool result = !digitalRead(inputPin); // Invert input signal. Pullup is active low. 1 = off. 0 = on.
+
+  if(oldResult != result) 
+  {
+    if(result) 
+    {
+      Serial.println(result);
+      
+      // Send keypress
+      buf[2] = 26;
+      Serial.write(buf, 8);   
+    } 
+    else 
+    {
+      Serial.println(result);
+      
+       // Send release keypress
+      buf[0] = 0;
+      buf[2] = 0;
+      Serial.write(buf, 8); 
+    }  
+  }
   
+  oldResult = result;
   digitalWrite(LED_BUILTIN, result);
-  Serial.println(result);
 }
