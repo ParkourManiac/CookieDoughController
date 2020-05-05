4fcabbeace20bafb9c12752bd955e6dccb56dbe0
TE4-Daniel-Kull <daniel.kull@elev.ga.ntig.se>
2020-05-05 15:58:02 +0200

    Added makefile. WIP test framwork TestCookieDough

---
+++ main-program/TestCookieDough/test.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void RunTests() {
+    printf("Running tests...\n");
+    // TODO: Run methods containing tests.
+}
+
+void PrintResults() {
+    printf("Results: \n");
+    // Print how many failed and how many succeded.
+}
+
+int main() {
+    printf("-------------------\n");
+    RunTests();
+    printf("-------------------\n");
+    PrintResults();
+    printf("-------------------\n");
+}
