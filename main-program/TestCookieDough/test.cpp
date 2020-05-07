9d91d97918bc9777569af1014c64295bf23550e2
TE4-Daniel-Kull <daniel.kull@elev.ga.ntig.se>
2020-05-07 12:39:34 +0200

    Documentation added

--- main-program/TestCookieDough/test.cpp
+++ main-program/TestCookieDough/test.cpp
@@ -1,6 +1,9 @@
 #include "test.h"
 #include "FrameworkTest.cpp"
 
+/**
+ * @brief Stores useful information about a test result.
+ */
 struct test_result 
 {
     bool passed;
@@ -9,10 +12,20 @@ struct test_result
     const char* code;
 };
 
+/**
+ * @brief The max amount of test we can write.
+ * Note: Just increase this value if more tests are needed.
+ */
 #define AMOUNT_OF_TESTS 100
 int currentTestIndex = 0;
+/**
+ * @brief Contains the result of each test that has been run.
+ */
 test_result results[AMOUNT_OF_TESTS];
 
+/**
+ * @brief Prints the result of all tests and provides information about failed tests.
+ */
 void PrintResults() 
 {
     printf("Results: \n");
@@ -47,6 +60,15 @@ void PrintResults()
     }
 }
 
+/**
+ * @brief Used to convert a test evalutation into a test result.
+ * WARNING: THIS IS NOT TO BE USED. USE MACRO 'ASSERT_TEST(condition)' INSTEAD.
+ * 
+ * @param eval The condition to be tested.
+ * @param code The code of the evaluation.
+ * @param file The file in which the test is defined.
+ * @param line The line number from where this function was called.
+ */
 void Test(bool eval, const char* code, const char* file, unsigned long line) 
 {
     test_result newResult;
@@ -60,15 +82,27 @@ void Test(bool eval, const char* code, const char* file, unsigned long line)
     currentTestIndex++;
 }
 
+/**
+ * @brief Handles running the tests and printing the result. 
+ */
 int main() {
     printf("\n-------------------\n");
+    printf("Running tests:\n");
     RunTests();
     PrintResults();
 }
 
+/**
+ * @brief A container for all tests to be run.
+ * 
+ * NOTE: Write your tests here! USE MACRO 'RUN_TEST(NameOfFunction)'. 
+ * Example 'RUN_TEST(MyFunction)' will run the function 'void MyFunction();'.
+ * 
+ * TESTS: When writing a new test make sure to use the 'ASSERT_TEST(condition)' macro to properly run the test. 
+ * Example 'void ExampleTest() { ASSERT_TEST((2+2) == 4) }'.
+ */
 void RunTests() 
 {
-    printf("Running tests:\n");
     RUN_TEST(AddTwoNumbers_SumIsCorrect);
     RUN_TEST(AddTwoNumbers_TestFails);
 }
