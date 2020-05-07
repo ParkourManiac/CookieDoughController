9467d5cad1a6912f1e9e95c1b9e95f7b81671a5b
Daniel Kull <35598620+TE4-Daniel-Kull@users.noreply.github.com>
2020-05-07 12:52:55 +0200

    Update test.h

--- main-program/TestCookieDough/test.h
+++ main-program/TestCookieDough/test.h
@@ -3,10 +3,12 @@
 
 /**
  * @brief Tests if a condition is successful.
+ * Example: 'void MyFunction() { ASSERT_TEST((2+2) == 4); }'
  */
 #define ASSERT_TEST(condition) Test(condition, #condition, __FILE__, __LINE__)
 /**
  * @brief Runs the function provided as a test.
+ * Example: 'RUN_TEST(MyFunction);'
  */
 #define RUN_TEST(functionName) \
 printf("\033[0;33m"); \
