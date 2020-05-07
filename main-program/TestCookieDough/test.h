793c5b52edde4a06aa66bf944e9a8cd00c61c8b4
TE4-Daniel-Kull <daniel.kull@elev.ga.ntig.se>
2020-05-07 12:49:38 +0200

    Documented macros

--- main-program/TestCookieDough/test.h
+++ main-program/TestCookieDough/test.h
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * @brief Tests if a condition is successful.
+ */
 #define ASSERT_TEST(condition) Test(condition, #condition, __FILE__, __LINE__)
-#define RUN_TEST(name) \
+/**
+ * @brief Runs the function provided as a test.
+ */
+#define RUN_TEST(functionName) \
 printf("\033[0;33m"); \
-printf(#name); \
+printf(#functionName); \
 printf("\033[0m" "\n"); \
-name();
+functionName();
 
 void RunTests();
 void PrintResults();
