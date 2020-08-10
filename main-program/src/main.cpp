6d6700cebd7fcb495641496dc610b37de715b367
ParkourManiac <daniel.kull1@gmail.com>
2020-08-10 20:42:42 +0200

    Added documentation.

--- main-program/src/main.cpp
+++ main-program/src/main.cpp
@@ -2,6 +2,16 @@
 
 #include <Controller.h>
 
+// TODO: Copy this description into the readme file of the project.
+/**
+ * @brief A pre-defined controller object, set up with the prefered keymap settings.
+ * 
+ * @note To configure the controller and run the program:
+ * 1. Create an external config file ending with the extension ".cpp" in the src folder.
+ * 2. Define a variable of type "Controller" called "controller".
+ * 3. Call the constructor of the "Controller" object with your prefered default and special function keymap.
+ * This will configure the controller to use your own custom configuration.
+ */
 extern Controller controller;
 
 // PROGRAM
